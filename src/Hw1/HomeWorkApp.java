d69a568c4dafbb6417183c5b3dbf48491af58c32
Dmitry2406 <gorunov5@ya.ru>
2021-06-17 13:56:51 +0300

    Задание 1 и 2

--- src/Hw1/HomeWorkApp.java
+++ src/Hw1/HomeWorkApp.java
@@ -2,6 +2,13 @@ package Hw1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("_Orange");
+        System.out.println("_Banana");
+        System.out.println("_Apple");
+
     }
 }
